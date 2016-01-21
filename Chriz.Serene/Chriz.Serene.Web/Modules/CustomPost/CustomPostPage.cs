1919e7c3a68fb1be631fdd5e9aa617bd048159b6
Chris <fzhenmei@gmail.com>
2016-01-21 09:58:36 +0800

    增加一个普通 MVC Controller 进行测试。增加Web Test 项目，不过不够灵活和方便，Firefox的 HttpRequester 插件更加方便灵活。

---
+++ Chriz.Serene/Chriz.Serene.Web/Modules/CustomPost/CustomPostPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Web.Mvc;
+
+namespace Chriz.Serene.CustomPost.Pages
+{
+    [RoutePrefix("CustomPost"), Route("{action=index}")]
+    public class CustomPostController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("OK");
+        }
+
+        [HttpPost]
+        public ActionResult DoPost()
+        {
+            try
+            {
+                var req = Request.InputStream;
+                req.Seek(0, SeekOrigin.Begin);
+                var content = new StreamReader(req).ReadToEnd();
+                return Content(content);
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
